d04e00b07f8bfd1c5bfc49c4178f217dc6a1aa0a
timon <982753566@qq.com>
2020-06-15 15:31:53 +0800

    登录页面静态制作 、input的数据校验

--- vue-admin/src/views/Login/index.vue
+++ vue-admin/src/views/Login/index.vue
@@ -1,32 +1,239 @@
 <!--  -->
 <template>
-<div id="login">
-    <h1>这里是登录页</h1>
-</div>
+  <div id="login">
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          v-for="item in menuTab"
+          :key="item.id"
+          :class="{ current: item.current }"
+          @click="toggleMenu(item)"
+        >
+          {{ item.txt }}
+        </li>
+      </ul>
+      <!-- 表单 -->
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        class="login-form"
+        size="medium"
+      >
+        <el-form-item prop="username" class="item-form">
+          <label>邮箱</label>
+          <el-input
+            type="text"
+            v-model="ruleForm.username"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item prop="password" class="item-form">
+          <label>密码</label>
+          <el-input
+            type="password"
+            v-model="ruleForm.password"
+            autocomplete="off"
+            minlength="6"
+            maxlength="20"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item
+          prop="passwords"
+          class="item-form"
+          v-if="model === 'register'"
+        >
+          <label>重复密码</label>
+          <el-input
+            type="text"
+            v-model="ruleForm.passwords"
+            autocomplete="off"
+            minlength="6"
+            maxlength="20"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item prop="code" class="item-form">
+          <label for="">验证码</label>
+          <el-row :gutter="10">
+            <el-col :span="15">
+              <el-input
+                v-model="ruleForm.code"
+                minlength="6"
+                maxlength="6"
+              ></el-input>
+            </el-col>
+            <el-col :span="9">
+              <el-button type="success" class="block">获取验证码</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            type="danger"
+            @click="submitForm('ruleForm')"
+            class=" login-btn block"
+            >提交</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
+import {
+  stripscript,
+  validateEmail,
+  validatePass,
+  validateVCode,
+} from "@/utils/validate";
 export default {
-data() {
-return {
+  data() {
+    let validateUsername = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入用户名"));
+      } else if (validateEmail(value)) {
+        callback(new Error("用户名格式有误"));
+      } else {
+        callback(); //true
+      }
+    };
+    // 验证密码
+     let validatePassword = (rule, value, callback) => {
+      // 过滤后的数据
+      ruleForm.password = stripscript(value);
+      value = ruleForm.password;
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else if (validatePass(value)) {
+        callback(new Error("密码为6至20位数字+字母"));
+      } else {
+        callback();
+      }
+    };
+    // 验证重复密码
+    let validatePasswords = (rule, value, callback) => {
+      // 如果模块值为login, 直接通过
+      if (model.value === "login") {
+        callback();
+      }
+      // 过滤后的数据
+      ruleForm.passwords = stripscript(value);
+      value = ruleForm.passwords;
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value != ruleForm.password) {
+        callback(new Error("重复密码不正确"));
+      } else {
+        callback();
+      }
+    };
+    // 验证验证码
+    let validateCode = (rule, value, callback) => {
+      if (value === "") {
+        return callback(new Error("请输入验证码"));
+      } else if (validateVCode(value)) {
+        return callback(new Error("验证码格式有误"));
+      } else {
+        callback();
+      }
+    };
 
-}
-},
-//生命周期 - 创建完成（访问当前this实例）
-created() {
+    return {
+      menuTab: [
+        { txt: "登录", current: true, type: "login" },
+        { txt: "注册", current: false, type: "register" },
+      ],
+      ruleForm: {
+        username: "",
+        password: "",
+        passwords: "",
+        code: "",
+      },
+      model: "login",
+      rules: {
+        username: [{ validator: validateUsername, trigger: "blur" }],
+        password: [{ validator: validatePassword, trigger: "blur" }],
+        passwords: [{ validator: validatePasswords, trigger: "blur" }],
+        code: [{ validator: validateCode, trigger: "blur" }],
+      },
+    };
+  },
+  methods: {
+    toggleMenu(data) {
+      this.menuTab.forEach((elem, index) => {
+        elem.current = false;
+      });
+      data.current = true;
 
-},
-//生命周期 - 挂载完成（访问DOM元素）
-mounted() {
-
-}
-}
+      this.model = data.type;
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+  },
+  //生命周期 - 创建完成（访问当前this实例）
+  created() {
+     
+  },
+  //生命周期 - 挂载完成（访问DOM元素）
+  mounted() {
+       console.log(this.ruleForm.username)
+  },
+};
 </script>
 <style lang="scss" scoped>
-/* @import url(); 引入css类 */
-#login{
-    height: 100vh;
-    background-color: #344a5f;
-
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #fff;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+  .current {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+}
+.login-form {
+  margin-top: 29px;
+  label {
+    display: block;
+    margin-bottom: 3px;
+    font-size: 14px;
+    color: #fff;
+  }
+  .item-form {
+    margin-bottom: 13px;
+  }
+  .block {
+    display: block;
+    width: 100%;
+  }
+  .login-btn {
+    margin-top: 19px;
+  }
 }
 </style>
